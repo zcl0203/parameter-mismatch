271bf46507ee005f25382200b4c0dbdd1f04132a
zcl <1399892596@qq.com>
2019-01-03 21:33:26 +0800

    extract type

--- extractType.js
+++ extractType.js
@@ -2,36 +2,59 @@ var fs = require('fs');
 var path = require('path');
 
 var call_func_file = 'call_func_info.json';
-var call_func_info = JSON.parse(fs.readFileSync(path.join(process.cwd(), call_func_file)));
-var type = ['String', 'Number', 'Boolean', 'Object', 'Array', 'Function', 'Optional', 'optional'];
+var call_func_info = JSON.parse(fs.readFileSync(path.join(process.cwd(), call_func_file)), 'utf-8');
 var call_func_type_file = 'call_func_type.json';
+var type = ['string', 'number', 'boolean', 'object', 'array', 'function', 'optional'];
+var returnWords = ['return', 'returns'];
+var call_func_type_info = [];
 
 for (var call_func of call_func_info) {
 
     var func_info = call_func.func_info;
+
     if (func_info.params && func_info.params.length && func_info.comment.length) {
 
-        var params = [];
+        var params = [],
+            retType = [];
         for (var param of func_info.params) {
             var name = param,
                 tmp = [];
 
             for (var comm of func_info.comment) {
-                if (comm.indexOf(param) !== -1) {
+                var ret = false;
+                for (var retword of returnWords) {
+                    if (comm.toLowerCase().indexOf(retword) !== -1) {
+                        ret = true;
+                    }
+                }
+                if (ret) {
                     for (var t of type) {
-                        if (comm.indexOf(t) !== -1) {
-                            tmp.push(t);
+                        if (comm.toLowerCase().indexOf(t) !== -1) {
+                            if (retType.indexOf(t) === -1) {
+                                retType.push(t);
+                            }
+                        }
+                    }
+                } else if (comm.indexOf(param) !== -1) {
+                    for (var t of type) {
+                        if (comm.toLowerCase().indexOf(t) !== -1) {
+                            if (tmp.indexOf(t) === -1) {
+                                tmp.push(t);
+                            }
                         }
                     }
                 }
             }
+
             params.push({
                 name: name,
                 type: tmp
             });
         }
         call_func.func_info.params = params;
+        call_func.func_info.return_type = retType;
+        call_func_type_info.push(call_func);
     }
 }
 
-fs.writeFileSync(path.join(process.cwd(), call_func_type_file), JSON.stringify(call_func_info), 'utf-8');
+fs.writeFileSync(path.join(process.cwd(), call_func_type_file), JSON.stringify(call_func_type_info), 'utf-8');
