3dea0f03751a93c1a15e0ef6701f0f4fd5c7387c
zcl <1399892596@qq.com>
2019-01-17 09:42:10 +0800

    extract type

--- extractType.js
+++ extractType.js
@@ -10,6 +10,7 @@ var call_func_type_info = [];
 
 for (var call_func of call_func_info) {
 
+    var call_info = call_func.call_info;
     var func_info = call_func.func_info;
 
     if (func_info.params && func_info.params.length && func_info.comment.length) {
@@ -55,6 +56,41 @@ for (var call_func of call_func_info) {
         call_func.func_info.return_type = retType;
         call_func_type_info.push(call_func);
     }
+
+    if (call_info.args && call_info.args.length && call_info.comment) {
+        var args = [];
+        for (var param of call_info.args) {
+            var name = param,
+                tmp = [];
+
+            for (var comm of call_info.comment) {
+                var ret = false;
+                for (var retword of returnWords) {
+                    if (comm.toLowerCase().indexOf(retword) !== -1) {
+                        ret = true;
+                    }
+                }
+               if (comm.indexOf(param) !== -1) {
+                    for (var t of type) {
+                        if (comm.toLowerCase().indexOf(t) !== -1) {
+                            if (tmp.indexOf(t) === -1) {
+                                tmp.push(t);
+                            }
+                        }
+                    }
+                }
+            }
+
+            args.push({
+                name: name,
+                type: tmp
+            });
+        }
+        call_func.call_info.args = args;
+        call_func.call_info.return_type = retType;
+        call_func_type_info.push(call_func);
+    }
 }
 
 fs.writeFileSync(path.join(process.cwd(), call_func_type_file), JSON.stringify(call_func_type_info), 'utf-8');
+
